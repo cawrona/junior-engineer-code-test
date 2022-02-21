2e10f2942aaaad16b420aa33c0385237af32d667
Christian <christian.wrona@gmail.com>
2022-02-21 10:18:37 -0500

    Fixed: Date handling and posting logic. Also, migration.

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,27 @@
-import datetime
-from django.db import models
+# Looks like this issue has a few layers. Currently, our datetime storage is timezone naive
+# which doesn't mix well with django's default timezone aware objects such as the auto_now field.
+# It seems like an attempt was made to override auto_now to convert utc to localtime, however this
+# is not allowed by django. My first thought would be to store all times as timezone aware UTC in django then use
+# javascript to display client-side times in their local time, though this may be beyond the scope of
+# this bug and the pytest. For now, I will implement a band-aid fix that stores our dates as local times.
 
-def auto_now():
-    return datetime.datetime.now() - datetime.timedelta(days=1)
+from django.db import models
+from django.utils import timezone
+
+# Removed auto_now "override"
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(max_length=2048)
     posted_by = models.CharField(max_length=50, default="root")
-    posted_at = models.DateTimeField(default=auto_now)
-    edited_at = models.DateTimeField(auto_now=True)
+    posted_at = models.DateTimeField(editable=False)    # "auto-fill" moved to save() override
+    edited_at = models.DateTimeField(editable=False)    # editable=False from auto_now, but this time without the UTC confusion.
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:                                     # on create, set posted time to current time
+            self.posted_at = timezone.now().astimezone()
+        self.edited_at = timezone.now().astimezone()        # on update, set edit time to current time
+        return super(Post, self).save(*args, **kwargs)
